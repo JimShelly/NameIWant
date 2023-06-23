9cc3444b612c377cb10390daf864afca4238a2d1
Jim Shelly <james.michaelshelly@fullstackacademy.com>
2023-06-22 19:08:45 -0500

    Changed something

--- block02/code.js
+++ block02/code.js
@@ -2,4 +2,4 @@ console.log('Hello Fullstack!');
 console.log('Next step');
 console.log('This is an edit');
 console.log('added in main branch');
-console.log('Add change to MyBranch');
+console.log('Add a change to MyBranch');
